9a9a2399858c4f9be22a6ba133f315704bc4928b
tjikaljedy <tjikaljedy@gmail.com>
2024-04-25 02:21:16 +0700

    update doc-besu

--- docs-besu/src/main/java/io/melody/besu/BesuApplication.java
+++ docs-besu/src/main/java/io/melody/besu/BesuApplication.java
@@ -1,8 +1,9 @@
 package io.melody.besu;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+@SpringBootApplication
 public class BesuApplication {
   public static void main(String[] args) {
     SpringApplication.run(BesuApplication.class, args);
